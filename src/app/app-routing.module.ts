720a6716df1a37cd595c23997b84288a101301f0
Justin Kim <mletemps@hotmail.com>
2022-02-21 19:20:37 -0500

    added route for statistics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import {NavigationComponent} from "src/app/component/navigation/navigation.component";
 import {GameComponent} from "src/app/component/game/game.component";
+import {StatisticsComponent} from "src/app/component/statistics/statistics.component";
 const routes: Routes = [
   // {path: "", component: NavigationComponent }
-  {path: "", component: GameComponent }
+  {path: "", component: GameComponent },
+  {path: "statistics", component: StatisticsComponent }
 ];
 
 @NgModule({
