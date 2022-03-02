5ca702545233fb97be249b8f68685a536dbc72fe
unknown <mletemps@hotmail.com>
2022-03-02 09:19:50 -0500

    added some styling and implemented some filtering logic

--- src/app/component/valid-words/valid-words.component.ts
+++ src/app/component/valid-words/valid-words.component.ts
@@ -1,21 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadWordsService } from 'src/services/load-words.service';
+import { v4 as uuidv4 } from 'uuid';
+
+
 
 @Component({
   selector: 'app-valid-words',
   templateUrl: './valid-words.component.html',
   styleUrls: ['./valid-words.component.scss']
 })
+
 export class ValidWordsComponent implements OnInit {
 
-  constructor(private wordleWord: LoadWordsService) { }
+  
+  constructor(private allWords: LoadWordsService) { }
 
-  wordleWords: any;
+  // wordleWords: any = {} as words;
+  wordleWords: any = [];
   ngOnInit(): void {
-    this.wordleWords = this.wordleWord.getWords();
-    // console.log(this.wordleWords);
+    // this.wordleWords = this.allWords.getWords();
+    for (var i = 0; i < this.allWords.getWords().length; i++)
+    {
+      this.wordleWords[i] = {};
+      this.wordleWords[i].wordle = this.allWords.getWords()[i].wordle.toString();
+      this.wordleWords[i].key = uuidv4();
+      
+ 
+    }
+    this.contains("a");
+    this.doesNotContain("b");
+    
   }
 
+  contains(c: string)
+  {
+    console.log(c);
+    this.wordleWords = this.wordleWords.filter((e: { wordle: string | any[]; }) =>{
+      return e.wordle.includes(c);
 
+    });
+  }
+
+  doesNotContain(c: string)
+  {
+    console.log(c);
+    this.wordleWords = this.wordleWords.filter((e: { wordle: string | any[]; }) =>{
+      return !e.wordle.includes(c);
+
+    });
+  }
 
 }
